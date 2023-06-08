98d5b6f205689f9001b9ab7e8ffd8b3e00e1aeb6
Aardra R Nair <72698208+Aardra28@users.noreply.github.com>
2023-06-08 14:18:17 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,11 @@
 import streamlit as st
 st.title("house_expenditure")
+import streamlit as st
+import numpy as np
+import pandas as pd
+
+chart_data = pd.DataFrame(
+     np.random.randn(20, 3),
+     columns=['a', 'b', 'c'])
+
+st.line_chart(chart_data)
