790be5694a84e2bf08d9bc82a4d0a6554c50c238
Aardra R Nair <72698208+Aardra28@users.noreply.github.com>
2023-06-08 14:38:28 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,29 +9,53 @@ st.title("house_expenditure")
 #data
 
 # Read the CSV file
+import tkinter as tk
+import pandas as pd
+
+# Read the expense data from CSV
 df = pd.read_csv('house_building_expenditure.csv')
 
-# Get the date and expense columns
-date_col = df['Date']
-expense_col = df['Expense']
+# Function to calculate total expenses and display in GUI
+def calculate_expenses():
+    selected_start_date = start_date_entry.get()
+    selected_end_date = end_date_entry.get()
+    selected_attribute = attribute_entry.get()
+    
+    selected_expenses = df[(df['Date'] >= selected_start_date) & (df['Date'] <= selected_end_date) & (df['Attribute'] == selected_attribute)]
+    total_expenses = selected_expenses['Expense'].sum()
+    
+    result_label.config(text=f"Total expenses of {selected_attribute} from {selected_start_date} to {selected_end_date}: {total_expenses}")
 
-# Get the selected date or range of dates
-selected_date = input('Enter a date (YYYY-MM-DD): ')
-selected_range = input('Enter a range of dates (YYYY-MM-DD): ')
+# Create GUI window
+window = tk.Tk()
+window.title("Expense Calculator")
 
-# Get the total expense for the selected date or range of dates
-if selected_date:
-    total_expense = df[df['Date'] == selected_date]['Expense'].sum()
-elif selected_range:
-    total_expense = df[df['Date'].between(selected_range[0], selected_range[1])]['Expense'].sum()
+# Date range input fields
+start_date_label = tk.Label(window, text="Start Date (YYYY-MM-DD): ")
+start_date_label.pack()
+start_date_entry = tk.Entry(window)
+start_date_entry.pack()
 
-# Plot the expenses
-plt.plot(date_col, expense_col)
-plt.xlabel('Date')
-plt.ylabel('Expense')
-plt.title('Expenses')
-plt.show()
+end_date_label = tk.Label(window, text="End Date (YYYY-MM-DD): ")
+end_date_label.pack()
+end_date_entry = tk.Entry(window)
+end_date_entry.pack()
 
-# Print the total expense
-print('Total expense:', total_expense)
+# Attribute input field
+attribute_label = tk.Label(window, text="Attribute: ")
+attribute_label.pack()
+attribute_entry = tk.Entry(window)
+attribute_entry.pack()
+
+# Calculate button
+calculate_button = tk.Button(window, text="Calculate", command=calculate_expenses)
+calculate_button.pack()
+
+# Result label
+result_label = tk.Label(window, text="")
+result_label.pack()
+
+# Start GUI event loop
+window.mainloop()
+
 
