3a421df49ff7d174b75ee89b046896ac0f12cf23
Aardra R Nair <72698208+Aardra28@users.noreply.github.com>
2023-06-08 14:21:50 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,24 +2,5 @@ import streamlit as st
 import numpy as np
 import pandas as pd
 st.title("house_expenditure")
-
-import streamlit as st
-import numpy as np
-import pandas as pd
-
-map_data = pd.DataFrame(
-    np.random.randn(1000, 2) / [50, 50] + [37.76, -122.4],
-    columns=['lat', 'lon'])
-
-st.map(map_data)
-
-import streamlit as st
-x = st.slider('x')  # 👈 this is a widget
-st.write(x, 'squared is', x * x)
-
-import streamlit as st
-st.text_input("Your name", key="name")
-
-# You can access the value at any point with:
-st.session_state.name
+pd=read_csv("
 
