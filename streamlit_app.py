ccb8bdaede2571d0a2c7c6b5ff196f9b0b12a9e0
Aardra R Nair <72698208+Aardra28@users.noreply.github.com>
2023-06-08 14:42:25 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,54 +9,40 @@ st.title("house_expenditure")
 #data
 
 
-# Read the CSV file
-import tkinter as tk
+import streamlit as st
 import pandas as pd
 
 # Read the expense data from CSV
-df = pd.read_csv('house_building_expenditure.csv')
+df = pd.read_csv('expenses.csv')
 
-# Function to calculate total expenses and display in GUI
+# Function to calculate total expenses and display in Streamlit
 def calculate_expenses():
-    selected_start_date = start_date_entry.get()
-    selected_end_date = end_date_entry.get()
-    selected_attribute = attribute_entry.get()
+    selected_start_date = start_date_input.value
+    selected_end_date = end_date_input.value
+    selected_attribute = attribute_input.value
     
     selected_expenses = df[(df['Date'] >= selected_start_date) & (df['Date'] <= selected_end_date) & (df['Attribute'] == selected_attribute)]
     total_expenses = selected_expenses['Expense'].sum()
     
-    result_label.config(text=f"Total expenses of {selected_attribute} from {selected_start_date} to {selected_end_date}: {total_expenses}")
+    result_text = f"Total expenses of {selected_attribute} from {selected_start_date} to {selected_end_date}: {total_expenses}"
+    result_text = f"**{result_text}**"
+    st.markdown(result_text)
 
-# Create GUI window
-window = tk.Tk()
-window.title("Expense Calculator")
+# Create Streamlit app
+st.title("Expense Calculator")
 
 # Date range input fields
-start_date_label = tk.Label(window, text="Start Date (YYYY-MM-DD): ")
-start_date_label.pack()
-start_date_entry = tk.Entry(window)
-start_date_entry.pack()
-
-end_date_label = tk.Label(window, text="End Date (YYYY-MM-DD): ")
-end_date_label.pack()
-end_date_entry = tk.Entry(window)
-end_date_entry.pack()
-
-# Attribute input field
-attribute_label = tk.Label(window, text="Attribute: ")
-attribute_label.pack()
-attribute_entry = tk.Entry(window)
-attribute_entry.pack()
+start_date_input = st.text_input("Start Date (YYYY-MM-DD):")
+end_date_input = st.text_input("End Date (YYYY-MM-DD):")
+attribute_input = st.text_input("Attribute:")
 
 # Calculate button
-calculate_button = tk.Button(window, text="Calculate", command=calculate_expenses)
-calculate_button.pack()
+calculate_button = st.button("Calculate", on_click=calculate_expenses)
 
-# Result label
-result_label = tk.Label(window, text="")
-result_label.pack()
+# Result area
+st.markdown("---")
+result_text = "Total expenses will be displayed here."
+st.markdown(result_text)
 
-# Start GUI event loop
-window.mainloop()
 
 
