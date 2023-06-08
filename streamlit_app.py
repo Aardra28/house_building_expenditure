13e71d5f6ac5f4fffe5139099b1561c0aa5cb0d2
Aardra R Nair <72698208+Aardra28@users.noreply.github.com>
2023-06-08 14:19:41 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,11 +1,25 @@
 import streamlit as st
+import numpy as np
+import pandas as pd
 st.title("house_expenditure")
+
 import streamlit as st
 import numpy as np
 import pandas as pd
 
-chart_data = pd.DataFrame(
-     np.random.randn(20, 3),
-     columns=['a', 'b', 'c'])
+map_data = pd.DataFrame(
+    np.random.randn(1000, 2) / [50, 50] + [37.76, -122.4],
+    columns=['lat', 'lon'])
 
-st.line_chart(chart_data)
+st.map(map_data)
+
+import streamlit as st
+x = st.slider('x')  # 👈 this is a widget
+st.write(x, 'squared is', x * x)
+
+import streamlit as st
+st.text_input("Your name", key="name")
+
+# You can access the value at any point with:
+st.session_state.name
+
