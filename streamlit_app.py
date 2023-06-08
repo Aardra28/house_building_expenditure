21d41c413ec5e4a43429b6daf265c5070b1445a6
Aardra R Nair <72698208+Aardra28@users.noreply.github.com>
2023-06-08 14:25:53 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,13 @@ import streamlit as st
 import numpy as np
 import pandas as pd
 st.title("house_expenditure")
-data=pd.read_csv("house_building_expenditure.csv")
-print(data)
+import requests
+
+url = 'https://raw.githubusercontent.com/user/repo/branch/path/to/file.csv'
+
+response = requests.get(url)
+
+if response.status_code == 200:
+    with open('file.csv', 'wb') as f:
+        f.write(response.content)
 
