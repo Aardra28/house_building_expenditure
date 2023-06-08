ec387a3bef38dfbab7725539b02b2e01f5d0412b
Aardra R Nair <72698208+Aardra28@users.noreply.github.com>
2023-06-08 14:27:52 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,13 +2,7 @@ import streamlit as st
 import numpy as np
 import pandas as pd
 st.title("house_expenditure")
-import requests
 
-url = 'house_building_expenditure.csv'
-
-response = requests.get(url)
-
-if response.status_code == 200:
-    with open('file.csv', 'wb') as f:
-        f.write(response.content)
-
+data=pd.read_csv("house_building_expenditure.csv")
+print(data)
+data
