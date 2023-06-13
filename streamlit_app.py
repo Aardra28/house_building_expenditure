db6e826bd55c6f32e8bfbb505dda046fe8588056
Aardra R Nair <72698208+Aardra28@users.noreply.github.com>
2023-06-13 10:03:18 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,8 @@ import pandas as pd
 import altair as alt
 import datetime
 
+[theme]
+base="dark"
 def main():
     navigation = st.sidebar.radio(options=["About", "Home", "Update"], label="Navigation", index=get_navigation_index())
 
